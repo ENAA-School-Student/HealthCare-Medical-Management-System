7378bc107ebd411c46dc178b278271d8c46c0678
Youssef <yousseferrachid@gmail.com>
2026-04-28 12:51:54 +0100

    add PatientMapper with ignore mappings and update method

--- src/main/java/com/healthcare/healthcare/mapper/PatientMapper.java
+++ src/main/java/com/healthcare/healthcare/mapper/PatientMapper.java
@@ -3,15 +3,24 @@ package com.healthcare.healthcare.mapper;
 import com.healthcare.healthcare.dto.PatientRequestDTO;
 import com.healthcare.healthcare.dto.PatientResponseDTO;
 import com.healthcare.healthcare.entity.Patient;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PatientMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "appointments", ignore = true)
+    @Mapping(target = "medicalRecord", ignore = true)
     Patient toEntity(PatientRequestDTO dto);
 
     PatientResponseDTO toDto(Patient patient);
 
     List<PatientResponseDTO> toDtos(List<Patient>patients);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "appointments", ignore = true)
+    @Mapping(target = "medicalRecord", ignore = true)
+    void updateEntityFromDto(PatientRequestDTO dto, @MappingTarget Patient patient);
 }
