2f26653b637aec0bb85d100c71cc5a279bdf4e0a
Youssef <yousseferrachid@gmail.com>
2026-04-29 10:57:16 +0100

    adding the sourse and the target for mapping

--- src/main/java/com/healthcare/healthcare/mapper/AppointmentMapper.java
+++ src/main/java/com/healthcare/healthcare/mapper/AppointmentMapper.java
@@ -11,11 +11,16 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface AppointmentMapper {
     @Mapping(source = "patientId", target = "patient.id")
-    @Mapping(source = "doctorId",target = "doctor.id")
+    @Mapping(source = "doctorId",  target = "doctor.id")
+    @Mapping(target = "id",        ignore = true)
     Appointment toEntity(AppointmentRequestDTO dto);
 
-    @Mapping(source = "patient.id", target = "patientId")
-    @Mapping(source = "doctor.id",target = "doctorId")
+    @Mapping(source = "patient.id",        target = "patientId")
+    @Mapping(source = "patient.nom",       target = "patientNom")
+    @Mapping(source = "patient.prenom",    target = "patientPrenom")
+    @Mapping(source = "doctor.id",         target = "doctorId")
+    @Mapping(source = "doctor.nom",        target = "doctorNom")
+    @Mapping(source = "doctor.specialite", target = "doctorSpecialite")
     AppointmentResponseDTO toDto(Appointment appointment);
 
     List<AppointmentResponseDTO> toDtos(List<Appointment> appointments);
