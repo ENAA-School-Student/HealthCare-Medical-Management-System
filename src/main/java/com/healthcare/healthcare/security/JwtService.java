efd3bc4fb4069f0657a392a490b62165d9dc3a53
Youssef <yousseferrachid@gmail.com>
2026-05-15 11:24:38 +0100

    fixing the error

--- src/main/java/com/healthcare/healthcare/security/JwtService.java
+++ src/main/java/com/healthcare/healthcare/security/JwtService.java
@@ -4,21 +4,24 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-    @Value("${jwt.secret}")
-    private static String SECRET_KEY ;
+    private static final  String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970" ;
     private static final long EXPIRATION_TIME = 3600000;
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
 
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder()
