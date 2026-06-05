0fafe248f577431e145d1963bda8acdd153b62bb
Youssef <yousseferrachid@gmail.com>
2026-06-05 09:56:32 +0100

    add the pagination to the list methods

--- src/main/java/com/healthcare/healthcare/repository/PatientRepository.java
+++ src/main/java/com/healthcare/healthcare/repository/PatientRepository.java
@@ -1,5 +1,6 @@
 package com.healthcare.healthcare.repository;
 
+import com.healthcare.healthcare.dto.PatientResponseDTO;
 import com.healthcare.healthcare.entity.Patient;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -9,6 +10,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 
 @Repository
@@ -23,4 +26,5 @@ public interface PatientRepository extends JpaRepository<Patient,Long> {
 
     @Query("SELECT p FROM Patient p WHERE LOWER(p.nom) LIKE LOWER(CONCAT('%', :name, '%'))")
     Page<Patient> findByNomContaining(@Param("name") String name, Pageable pageable);
+
 }
