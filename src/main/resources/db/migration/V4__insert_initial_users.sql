f53e6c043859a42f72c39c892710a6296b5bfd67
Youssef <yousseferrachid@gmail.com>
2026-06-15 15:53:20 +0100

    test4

--- src/main/resources/db/migration/V4__insert_initial_users.sql
+++ src/main/resources/db/migration/V4__insert_initial_users.sql
@@ -1,11 +1,6 @@
-INSERT INTO user (username, email, password) VALUES
-    ('john_doe', 'john@example.com', 'john123'),
-    ('alice_smith', 'alice@example.com', 'alice123'),
-    ('michael_brown', 'michael@example.com', 'michael123'),
-    ('sarah_jones', 'sarah@example.com', 'sarah123'),
-    ('david_wilson', 'david@example.com', 'david123'),
-    ('emma_taylor', 'emma@example.com', 'emma123'),
-    ('daniel_moore', 'daniel@example.com', 'daniel123'),
-    ('olivia_white', 'olivia@example.com', 'olivia123'),
-    ('james_martin', 'james@example.com', 'james123'),
-    ('sophia_clark', 'sophia@example.com', 'sophia123');
+INSERT INTO user (username, email, password, role) VALUES
+                                                       ('admin',         'admin@healthcare.com',   '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iKEOCDpSHgBwPTQm8KmpCj4KPVJC', 'ADMIN'),
+                                                       ('john_doe',      'john@example.com',       '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iKEOCDpSHgBwPTQm8KmpCj4KPVJC', 'PATIENT'),
+                                                       ('alice_smith',   'alice@example.com',      '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iKEOCDpSHgBwPTQm8KmpCj4KPVJC', 'PATIENT'),
+                                                       ('dr_haddad',     'haddad@healthcare.com',  '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iKEOCDpSHgBwPTQm8KmpCj4KPVJC', 'DOCTOR'),
+                                                       ('dr_tazi',       'tazi@healthcare.com',    '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iKEOCDpSHgBwPTQm8KmpCj4KPVJC', 'DOCTOR');
